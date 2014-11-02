37b621858fa9ab15177908d5c65c22a86230a67b
Jean-François B. <2589111+jfbu@users.noreply.github.com>
2014-11-02 10:07:03 +0100

    1.09i du 18 décembre
    vers midi
    
    1.09i 2013/12/18
    
      * \xintiiexpr is a variant of \xintexpr which is optimized to
      deal only with (long) integers, / does a euclidean quotient.
    
      * \xintnumexpr has been renamed \xintiexpr, (former name kept
      but destined to be removed) \xintiiexpr is actually closer to
      \numexpr, but with integer division the euclidean one.
    
      * the \xintexpr parser automatically inserts \number in front of
      count, dimen, and skip registers/variables, or \numexpr, etc..
      expressions, with a tacit multiplication implied if following a
      number or parenthesized expression.
    
      * \xintAssign admits an optional argument to specify the
      expansion type to be used [] (none), [o] (once), [oo] (twice),
      [f] (full), ... , (the default is [e] which means to use \edef).
    
      * xinttools defines (if the names have not already been
      assigned) \odef, \oodef, \fdef: \oodef which expands twice the
      macro replacement text is a faster alternative to \edef (when
      the expansion gives hundreds of digits).
    
      * some across the board slight efficiency improvement as a
      result of modifications of various types to ``fork'' macros
      and ``branching conditionals'' which are used internally.

--- .resettimestamps.sh
+++ .resettimestamps.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-touch -t 201312051709.44 README
-touch -t 201312051708.06 xint.dtx
-touch -t 201312051714.56 xint.ins
-touch -t 201312051716.39 xint.pdf
+touch -t 201312181138.05 README
+touch -t 201312181132.32 xint.dtx
+touch -t 201312181132.40 xint.ins
+touch -t 201312181133.10 xint.pdf
+touch -t 201312181220.12 xint.tds.zip
