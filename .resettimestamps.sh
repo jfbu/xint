cda718a2f04d28de457fa5e98ade681e928c93ee
Jean-François B. <2589111+jfbu@users.noreply.github.com>
2014-11-02 09:35:32 +0100

    1.04 du 25 avril 2013
       version 1.04 of xint (2013/04/25)
    
       This release has some bug fixes, important improvements in the
       xintseries package and its documentation, and a new component
       xintcfrac.sty devoted to continued fractions.
    
       1. bug fixes: \xintIrr {0} was a bad one. Apart from that,
       there were some inaccuracies in the documentation.
    
       2. the base package xint has new commands to help deal in an
       expandable way with token lists produced by some of the bundle
       macros. Also, the division routine is a bit faster, and a
       rounding macro has been added to the xintfrac package.
    
       3. xintseries: a new implementation of \xintPowerSeries, based
       on a Horner scheme, has greatly reduced a problem of denominator
       build-up. Also, the package has new macros, among them
       \xintRationalSeries is for computing partial sums with general
       term F(n) where F is a rational function and the macro is given
       the n->F(n)/F(n-1) function. This is especially designed for
       series of the exponential type, again to avoid a denominator
       build-up which made \xintSeries and \xintPowerSeries inefficient
       in such situations. The documentation has been correspondingly
       extended.
    
       4. xintcfrac: this is a new package to deal with matters of
       continued fractions. For example \xintCFrac is like amsmath
       \cfrac apart from a little detail: it first computes the
       continued fraction corresponding to a given fractional number!
       (and then feeds \cfrac with it; almost all package macros only
       deal with computations, not typesetting). The package includes
       commodities to specify the coefficients of the (possibly
       generalized) continued fraction as function of the index, to
       compute simple and centered continued fractions, to return the
       list of all convergents, etc...
    
       The xint.dtx source file self-extracts packages xint.sty,
       xintgcd.sty, xintfrac.sty, xintseries.sty and xintcfrac.sty (as
       well as xint.ins).
    
    Le 25 avril toujours, vers 16:45, entretemps je m'étais aperçu d'un
    certain nombre de problèmes.
    
    Hi, sorry for uploading the new 1.04 again after a few hours:
    style files unchanged, but the pdf doc had two bad typos. Could
    the catalog description be updated to make mention of the new
    xintcfrac.sty package dealing with continued fractions? thanks,
    Jean-Francois

--- .resettimestamps.sh
+++ .resettimestamps.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-touch -t 201304151111.10 README
-touch -t 201304151101.37 xint.dtx
-touch -t 201304151101.39 xint.ins
-touch -t 201304151101.41 xint.pdf
+touch -t 201304251644.06 README
+touch -t 201304251650.20 xint.dtx
+touch -t 201304251650.22 xint.ins
+touch -t 201304251650.27 xint.pdf
