f05d908ab37fac8907639bc9985b116039d016a5
Jean-François B. <2589111+jfbu@users.noreply.github.com>
2014-11-02 09:55:58 +0100

    1.09e du 29 octobre 2013
    New features:
    
     * the new syntax \xintFor #1 in {\xintrationals [f+g]} \do
       {stuff with #1} does an infinite repetition with #1 first set to
       f, then f+g, f+2g, ... where f and g are decimal numbers, or
       fractions, or macros expanding to such things. Also available:
        - \xintintegers where each #1 will be a
                   \numexpr <explicit number>\relax
        - \xintdimensions where each #1 will be a
                   \dimexpr <explicit dimension>\relax.
       In the latter case the dimensions are internally kept in sp
       units, there is no loss of accuracy due to conversion in pt's by
       \the.
    
     * one breaks out of the infinite loop with \xintBreakFor and
       \xintBreakForAndDo (preferably put inside the true or false
       branch of a conditional as provided by the package itself or the
       etoolbox package or the ifthen package; there is no question of
       complete expandability here, so ifthen is allowed).
    
     * also new: \xintifForFirst, \xintifForLast which are expandable
       conditionals to use in the replacement text of the \xintFor and
       \xintFor* loops. Can be nested as expected.
    
     * new expandable conditionals \xintifCmp and \xintifInt
    
    The documentation contains a completely expandable implementation
    of the Quick Sort algorithm, and a graphical illustration of its
    functioning. It also has a completely expandable prime test and
    dynamically constructed prime tables and other examples of use of
    the completely expandable \xintApplyUnbraced or non completely
    expandable \xintApplyInline and \xintFor, \xintFor*.

--- .resettimestamps.sh
+++ .resettimestamps.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-touch -t 201310222256.41 README
-touch -t 201310222259.26 xint.dtx
-touch -t 201310222300.23 xint.ins
-touch -t 201310222300.31 xint.pdf
+touch -t 201310291705.05 README
+touch -t 201310291757.48 xint.dtx
+touch -t 201310291800.40 xint.ins
+touch -t 201310291800.49 xint.pdf
