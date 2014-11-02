f3117082de5dd366699b994c4e7caf2e8ca265ef
Jean-François B. <2589111+jfbu@users.noreply.github.com>
2014-11-02 09:45:54 +0100

    1.06a du 9 mai 2013
       xint 1.06a (2013/05/09)
       1. Code improvements in various macros of the base bundle
       package xint.sty, especially those doing iterated loops.
       2. New utilities for expandable manipulations of lists:
              \xintNthElt, \xintCSVtoList, \xintRevWithBraces
       3. Changes in expansion policy.
       Macros from previous releases did only a double expansion of
       their arguments. The new policy:
           a. macro arguments which are constrained to obey the TeX
       bounds on numbers are now inserted inside a \numexpr..\relax,
       hence completely expanded, one may use count registers, even
       arithmetic operations, etc...
           b. macro arguments which are supposedly (long) numbers or
       fractions will be fully expanded (using \romannumeral-`0)
       rather than only twice expanded.
       4. No more exceptional cases (such as for zero or one) for
       the format of fractions returned by the xintfrac macros, they are
       always output with a denominator and trailing [n]: A/B[n]
       (except for \xintRaw and \xintIrr).

--- .resettimestamps.sh
+++ .resettimestamps.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-touch -t 201305081005.02 README
-touch -t 201305072257.11 xint.dtx
-touch -t 201305072257.44 xint.ins
-touch -t 201305072257.49 xint.pdf
+touch -t 201305090857.13 README
+touch -t 201305090851.59 xint.dtx
+touch -t 201305090852.02 xint.ins
+touch -t 201305090852.07 xint.pdf
