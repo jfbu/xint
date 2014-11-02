ff0f989f4b677a390a9027f5618e321884cb6eed
Jean-François B. <2589111+jfbu@users.noreply.github.com>
2014-11-02 10:09:17 +0100

    1.09k du 21 janvier 2014
    vers 21:45
    
    1.09k 2014/01/21
    
      * inside \xintexpr..\relax (and its variants) tacit multiplication now
      applies when an opening parenthesis is met while scanning a number or
      looking for some infix operator,
    
      * the " is accepted by the \xintexpr syntax for input of (big)
      hexadecimal numbers, possibly with a fractional hexadecimal part.
      Example:
    
             \xintexpr 16^5-("F75DE.0A8B9+"8A21.F5746+16^-5)\relax
    
      * first sections of the user manual re-organized with the aim to
      remove duplicates. Various other documentation issues addressed.

--- .resettimestamps.sh
+++ .resettimestamps.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-touch -t 201401091947.20 README
-touch -t 201401091914.30 xint.dtx
-touch -t 201401091945.53 xint.ins
-touch -t 201401091946.23 xint.pdf
-touch -t 201401091952.22 xint.tds.zip
+touch -t 201401222111.40 README
+touch -t 201401222108.09 xint.dtx
+touch -t 201401222108.37 xint.ins
+touch -t 201401222109.02 xint.pdf
