cb768cc6fe30a60dafcc2e4a6b9b35ae22fcac22
Jean-François B. <2589111+jfbu@users.noreply.github.com>
2014-11-02 09:47:36 +0100

    1.07a du 26 mai 2013
    Hi Robin. This is 1.07a which fixes a minor bug in the 1.07 I uploaded
    around midnight. I prefer to correct before the TL2013 deadline. I
    attach a renewed concise description for the catalog. This release is an
    important evolution and I have provided an announcement text. Best
    wishes, Jean-Francois The bundle provides three core packages xint,
    xintfrac and xintexpr (the latter loading the others) to allow algebraic
    operations on arbitrarily long numbers; all macros are compatible to
    expansion only. Three additional packages xintgcd, xintseries, xintcfrac
    provide applications. Usable with any flavor of TeX supporting the e-TeX
    extensions.
    
       Release 1.07a (2013/05/26) brings important additions:
    
       1. \xintexpr...\relax is an expandable parser of algebraic
       expressions involving the basic operations +,-,*,/, and ^ as infix
       operators, as well as ! for the factorial. It expands everything
       along the way and recognizes integers, decimal numbers, numbers in
       scientific notations, and arbitrary levels of parenthesizing.
    
       2. \xintfloatexpr...\relax is the same thing, the operations being
       excuted with a precision D, whose default is 16 digits, and may be
       set arbitrarily (theoretical maximum 32768).
    
       3. the other package macros now accept the scientific notation on
       input and \xintFloat outputs in floating point format with a given
       precision D either specified as an optional argument or globally set.
    
       The bundle now comprises the core constituents xint, xintfrac, and
       the latest xintexpr which loads the two previous ones, and auxiliary
       packages xintgcd, xintseries and xintcfrac.
    
       The macros are usable with any flavor of TeX with the e-TeX
       extensions. Users of LaTeX will do \usepackage{xintexpr}, others
       \input xintexpr.sty

--- .resettimestamps.sh
+++ .resettimestamps.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-touch -t 201305252303.58 README
-touch -t 201305252354.57 xint.dtx
-touch -t 201305252355.15 xint.ins
-touch -t 201305252355.22 xint.pdf
+touch -t 201305261141.17 README
+touch -t 201305261142.22 xint.dtx
+touch -t 201305261142.24 xint.ins
+touch -t 201305261142.31 xint.pdf
