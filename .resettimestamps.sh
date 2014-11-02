b5743f83caffb3b3af8c49ad34a19fea4461c232
Jean-François B. <2589111+jfbu@users.noreply.github.com>
2014-11-02 09:48:50 +0100

    1.08 du 7 juin 2013
       This is release 1.08.
       1. Square root extraction, both for floats, and in a variant
          for integers.
       2. New package xintbinhex.sty for expandable conversions of
          arbitrary length data to and from binary and hexadecimal.
       3. Various improvements in the documentation.

--- .resettimestamps.sh
+++ .resettimestamps.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-touch -t 201305261141.17 README
-touch -t 201305261142.22 xint.dtx
-touch -t 201305261142.24 xint.ins
-touch -t 201305261142.31 xint.pdf
+touch -t 201306072114.10 README
+touch -t 201306072116.36 xint.dtx
+touch -t 201306072116.42 xint.ins
+touch -t 201306072116.48 xint.pdf
