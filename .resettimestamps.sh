550317717a788797bd7adace1426cc910247a459
Jean-François B. <2589111+jfbu@users.noreply.github.com>
2014-11-02 10:09:50 +0100

    1.09ka du 5 février 2014
    vers 22:05
    
    pas d'annonce.

--- .resettimestamps.sh
+++ .resettimestamps.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-touch -t 201401222111.40 README
-touch -t 201401222108.09 xint.dtx
-touch -t 201401222108.37 xint.ins
-touch -t 201401222109.02 xint.pdf
+touch -t 201402052148.30 README
+touch -t 201402052153.23 xint.dtx
+touch -t 201402052141.50 xint.ins
+touch -t 201402052153.40 xint.pdf
