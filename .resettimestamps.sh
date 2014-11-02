16ff10a99f9b2524b72a143a8934c70d7d840853
Jean-François B. <2589111+jfbu@users.noreply.github.com>
2014-11-02 09:57:34 +0100

    1.09g du 22 novembre 2013
    envoyé sur CTAN le 23 novembre vers 13:47
    
    All macros from xint not devoted to big integer computations have
    been moved to a new constituent xinttools, which is automatically
    loaded by xint and may also be used independently.
    
    Expandable loops \xintloop and \xintiloop allowing nesting have
    been added to this xinttools package.
    
    bug fix: the \xintFor loops modified the value of \count 255. They
    now use their own count register. (they are among the utilities
    moved to the xinttools package)

--- .resettimestamps.sh
+++ .resettimestamps.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-touch -t 201311041354.54 README
-touch -t 201311041351.42 xint.dtx
-touch -t 201311041351.44 xint.ins
-touch -t 201311041351.51 xint.pdf
+touch -t 201311231253.49 README
+touch -t 201311231226.56 xint.dtx
+touch -t 201311231229.33 xint.ins
+touch -t 201311231229.42 xint.pdf
