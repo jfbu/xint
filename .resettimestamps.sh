7e588167c9867e12b9e427311e609cc19ba341e3
Jean-François B. <2589111+jfbu@users.noreply.github.com>
2014-11-02 09:44:08 +0100

    1.05a du 2 mai 2013
       Release 1.05a fixes a wrong affirmation and a broken link in
       the documentation of 1.05, and removes five or six strange
       \expandafter's from the code.

--- .resettimestamps.sh
+++ .resettimestamps.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-touch -t 201305011909.21 README
-touch -t 201305011944.40 xint.dtx
-touch -t 201305011945.33 xint.ins
-touch -t 201305011945.38 xint.pdf
+touch -t 201305021818.12 README
+touch -t 201305021737.52 xint.dtx
+touch -t 201305021814.56 xint.ins
+touch -t 201305021815.01 xint.pdf
