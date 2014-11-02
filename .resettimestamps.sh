6fb1017e3e5f8a65dafd927d3fd883e12c54099b
Jean-François B. <2589111+jfbu@users.noreply.github.com>
2014-11-02 09:44:54 +0100

    1.06 du 7 mai 2013
    8 mai: je corrige le README de 1.06 a posteriori (xintcfrac.sty, pas
    xintcfrac) pour éviter de propager l'erreur par copier-coller la
    prochaine fois.
    
    7 mai 2013
    
       xint 1.06 (2013/05/07)
       1. Code improvements in various macros of the base bundle
       package xint.sty, especially those doing iterated loops.
       2. New utilities for expandable manipulations of lists:
              \xintNthElt, \xintCSVtoList, \xintRevWithBraces
       3. Changes in expansion policy.
       Macros from previous releases did only a double expansion of
       their arguments. The new policy:
           a. macro arguments which are constrained to obey the TeX
       bounds on numbers are now inserted inside a \numexpr..\relax,
       hence completely expanded, one may use count registers, even
       arithmetic operations, etc...
           b. macro arguments which are supposedly (long) numbers or
       fractions will be fully expanded (using \romannumeral-`0)
       rather than only twice expanded.
    
       This last change allows in an easier manner to use the package
       macros as building blocks to define expandable macros
       arbitrarily chainable one within another.

--- .resettimestamps.sh
+++ .resettimestamps.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-touch -t 201305021818.12 README
-touch -t 201305021737.52 xint.dtx
-touch -t 201305021814.56 xint.ins
-touch -t 201305021815.01 xint.pdf
+touch -t 201305081005.02 README
+touch -t 201305072257.11 xint.dtx
+touch -t 201305072257.44 xint.ins
+touch -t 201305072257.49 xint.pdf
