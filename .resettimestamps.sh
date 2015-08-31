0772ff3c9fcc7ca1d4b2c6ee6bdece7796886179
Jean-François B. <2589111+jfbu@users.noreply.github.com>
2015-08-31 12:09:22 +0200

    1.1b du 31 août 2015 (commit du master xintdev e8185cf)
    sera tagué v1.1b

--- .resettimestamps.sh
+++ .resettimestamps.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
-touch -t 201503072109.00 CHANGES.html
-touch -t 201503072108.59 CHANGES.pdf
+# 1.1b du 31 août 2015 (commit master xintdev e8185cf)
+touch -t 201508311155.48 CHANGES.html
+touch -t 201508311155.48 CHANGES.pdf
 touch -t 201410212303.20 Makefile
-touch -t 201503072108.52 README
-touch -t 201503072108.59 README.html
-touch -t 201503072108.56 README.pdf
-touch -t 201503072108.52 sourcexint.pdf
-touch -t 201503072019.14 xint.dtx
-touch -t 201503072108.12 xint.ins
-touch -t 201503072108.44 xint.pdf
-touch -t 201503241959.14 xinttdszip-lsR
+touch -t 201508311155.44 README
+touch -t 201508311155.48 README.html
+touch -t 201508311155.46 README.pdf
+touch -t 201508311155.44 sourcexint.pdf
+touch -t 201508311153.57 xint.dtx
+touch -t 201508311155.06 xint.ins
+touch -t 201508311155.37 xint.pdf
+touch -t 201508311159.15 xinttdszip-lsR
