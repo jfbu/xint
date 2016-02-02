fb1d2de4810ecb7d305336664bcbed55122c453a
Jean-François B. <2589111+jfbu@users.noreply.github.com>
2016-02-02 22:24:17 +0100

    Les touch dans .resettimestamps.sh sont avec -ch et des guillemets.
    On branch master

--- .resettimestamps.sh
+++ .resettimestamps.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-touch -t 201511222304.03 CHANGES.html
-touch -t 201511222304.03 CHANGES.pdf
-touch -t 201509162256.23 Makefile
-touch -t 201511222303.59 README
-touch -t 201511222304.03 README.html
-touch -t 201511222304.01 README.pdf
-touch -t 201511222303.59 sourcexint.pdf
-touch -t 201511222303.12 xint.dtx
-touch -t 201511222303.21 xint.ins
-touch -t 201511222303.52 xint.pdf
-touch -t 201511222335.00 xinttdszip-lsR
+touch -ch -t 201511222304.03 "CHANGES.html"
+touch -ch -t 201511222304.03 "CHANGES.pdf"
+touch -ch -t 201509162256.23 "Makefile"
+touch -ch -t 201511222303.59 "README"
+touch -ch -t 201511222304.03 "README.html"
+touch -ch -t 201511222304.01 "README.pdf"
+touch -ch -t 201511222303.59 "sourcexint.pdf"
+touch -ch -t 201511222303.12 "xint.dtx"
+touch -ch -t 201511222303.21 "xint.ins"
+touch -ch -t 201511222303.52 "xint.pdf"
+touch -ch -t 201511222335.00 "xinttdszip-lsR"
