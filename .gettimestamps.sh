81dfb850f47ec67a494b27e6d5f7198b63be4c67
Jean-François B. <2589111+jfbu@users.noreply.github.com>
2016-12-17 23:01:16 +0100

    Nouveau gettimestamps.sh utilisant `git ls-files`.
    Changes to be committed:
    	deleted:    .gitignore
    
    Changes not staged for commit:

---
+++ .gettimestamps.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+# Mardi 02 février 2016
+
+# repris de celui de courant/ pour mon site perso. Toute dernière mouture qui
+# doit marcher sur Mac OS et sur GNU/Linux malgré les stat et touch
+# différents.
+
+# Samedi 17 décembre 2016
+
+# encore un converti à l'emploi de `git ls-files`.
+
+case `uname -s` in
+    "Linux" )
+        MYDATEFORTOUCH() {
+            date -d"$(stat -c %y "$1")" +%Y%m%d%H%M.%S
+        }
+        ;;
+    "Darwin" )
+        MYDATEFORTOUCH() {
+            stat -f %Sm -t %Y%m%d%H%M.%S "$1"
+        }
+        ;;
+    * )
+        MYDATEFORTOUCH() {
+            197001010000.00
+        }
+        ;;
+esac
+
+echo "#!/bin/sh" > .resettimestamps.sh
+
+IFS='
+'
+
+for file in `git ls-files`
+do
+    [ "$file" != ".resettimestamps.sh" ] && \
+	echo "touch -ch -t $(MYDATEFORTOUCH "$file") \"$file\"" >> .resettimestamps.sh
+done
+
+chmod u+x .resettimestamps.sh
