7138e7d0fc7547531c32fe7861eac7e8b9f8ffc1
Jean-François B. <2589111+jfbu@users.noreply.github.com>
2025-06-02 14:59:27 +0200

    Add index.html to serve as static website

--- .resettimestamps.sh
+++ .resettimestamps.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 touch -ch -t 202506021109.16 ".gettimestamps.sh"
+touch -ch -t 202506021512.47 ".nojekyll"
 touch -ch -t 202206112020.09 "CHANGES.html"
 touch -ch -t 202205241111.40 "Makefile"
 touch -ch -t 202206112018.43 "README.md"
+touch -ch -t 202506021512.51 "index.html"
 touch -ch -t 202206112018.40 "xint.dtx"
 touch -ch -t 202206112030.11 "xinttdszip-lsR"
