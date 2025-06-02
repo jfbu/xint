3a5e4aab7772a78b0903042ad6b24eb156c806f2
Jean-François B. <2589111+jfbu@users.noreply.github.com>
2025-06-02 11:12:35 +0200

    Trim out timestamp resetting for PDFs as we don't serve them here

--- .resettimestamps.sh
+++ .resettimestamps.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
-touch -ch -t 201612172258.48 ".gettimestamps.sh"
+touch -ch -t 202506021109.16 ".gettimestamps.sh"
 touch -ch -t 202206112020.09 "CHANGES.html"
 touch -ch -t 202205241111.40 "Makefile"
 touch -ch -t 202206112018.43 "README.md"
-touch -ch -t 202206112020.08 "sourcexint.pdf"
 touch -ch -t 202206112018.40 "xint.dtx"
-touch -ch -t 202206112019.28 "xint.pdf"
 touch -ch -t 202206112030.11 "xinttdszip-lsR"
