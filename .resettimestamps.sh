1edbf6c572f0fe16f745f57ad7a1f73ea4871c4d
Jean-François B. <2589111+jfbu@users.noreply.github.com>
2025-06-08 15:40:36 +0200

    Add .gitignore

--- .resettimestamps.sh
+++ .resettimestamps.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 touch -ch -t 202506021109.16 ".gettimestamps.sh"
+touch -ch -t 202506081558.34 ".gitignore"
 touch -ch -t 202506021512.47 ".nojekyll"
 touch -ch -t 202206112020.09 "CHANGES.html"
 touch -ch -t 202205241111.40 "Makefile"
-touch -ch -t 202206112018.43 "README.md"
+touch -ch -t 202506081558.21 "README.md"
 touch -ch -t 202506021526.32 "index.html"
 touch -ch -t 202206112018.40 "xint.dtx"
 touch -ch -t 202206112030.11 "xinttdszip-lsR"
